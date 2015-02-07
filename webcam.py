d6857da521fad5100dca60df2a3a0eaefd29cc50
Will Crichton <willcrichton@cmu.edu>
2015-02-07 00:46:09 -0500

    Added play sounds

--- webcam.py
+++ webcam.py
@@ -1,8 +1,25 @@
 import cv2
+import vlc
+import random
+from time import sleep
 from threading import Thread
 from Queue import Queue
 
 queue = Queue()
+
+instruments = [
+    'bass',
+    'bell',
+    'corny',
+    'drums',
+    'electro',
+    'pianobell',
+    'woody'
+]
+
+def play_sound(instrument, pitch):
+    path = 'sounds/%s_%s.mp3' % (instrument, pitch)
+    vlc.MediaPlayer(path).play()
 
 def analyze_frames():
     while True:
