780b76b478a5a8ca13e1744d71fe17cb8b8b0652
Will Crichton <willcrichton@cmu.edu>
2015-02-07 15:19:37 -0500

    ALMOST DONE?

--- webcam.py
+++ webcam.py
@@ -1,14 +1,16 @@
 import cv2
 import vlc
 import random
-from time import time
+from time import time, sleep
 from threading import Thread
 from collections import Counter
 from Queue import Queue
+import numpy as np
+import sys
 
-CALIBRATING = False
-TIME_BETWEEN_ROWS = 0.25
-NUM_ROWS = 6
+CALIBRATING = int(sys.argv[1])
+BEAT_TIME = 0.25
+NUM_ROWS = 8
 NUM_COLS = 4
 NUM_SAMPLES = 16
 SQUARE_WIDTH = 170
@@ -31,8 +33,8 @@ INSTRUMENTS = [
 '''
 
 INSTRUMENTS = [
-    'bass',
-    'electro',
+    'pianobell',
+    'pianobell',
     'pianobell'
 ]
 
@@ -43,20 +45,31 @@ frame = None
 
 def play_sound(instrument, pitch):
     path = 'sounds/%s_%s.mp3' % (instrument, pitch)
-    vlc.MediaPlayer(path).play()
+    player = vlc.MediaPlayer(path)
+    player.audio_set_volume(200)
+    player.play()
 
 def play_music():
     last_time = time()
     cur_row_index = 0
+    cur_beat = 0
 
     while True:
         cur_time = time()
         delta = cur_time - last_time
 
-        if delta < TIME_BETWEEN_ROWS: continue
-        cur_row = rows[cur_row_index]
+        if delta < BEAT_TIME: continue
 
-        print rows
+        if cur_beat % 2 == 0:
+            if cur_beat == 6:
+                play_sound('drums', 2)
+                play_sound('bass', 5)
+            else:
+                play_sound('drums', 2)
+                play_sound('bass', 2)
+
+        cur_beat = (cur_beat + 1) % 8
+        cur_row = rows[cur_row_index]
 
         for (instrument, pitch) in cur_row:
             play_sound(instrument, pitch)
@@ -67,7 +80,7 @@ def play_music():
 def classifyPixel(tuple):
     (blue, green, red) = tuple
     (maximum, middle, minimum) = sorted([blue, green, red], reverse=True)
-    if maximum - minimum < 30 or maximum < 1.2 * minimum:
+    if maximum - minimum < 30 or maximum < minimum * 1.2:
         return SQUARE_WHITE
     elif maximum == blue:
         return SQUARE_BLUE
@@ -82,7 +95,10 @@ def block_worker():
         colors = []
         for x in xrange(i * SQUARE_WIDTH, (i + 1) * SQUARE_WIDTH, 4):
             for y in xrange(j * SQUARE_HEIGHT, (j + 1) * SQUARE_HEIGHT, 4):
-                colors.append(classifyPixel(frame[y][x]));
+                color = classifyPixel(frame[y][x])
+
+                if color != SQUARE_WHITE:
+                    colors.append(color);
 
         results[(i, j)] = Counter(colors)
         queue.task_done()
@@ -95,18 +111,21 @@ def counts_to_row(counts, pitch):
 
     return row
 
-def main():
-    for i in range(0, NUM_ROWS):
-        rows.append([])
-
-    t = Thread(target=play_music)
+def spawn_thread(func):
+    t = Thread(target=func)
     t.daemon = True
     t.start()
 
-    for i in xrange(0, 16):
-        t = Thread(target=block_worker)
-        t.daemon = True
-        t.start()
+def main():
+    global rows
+
+    for i in range(0, NUM_ROWS):
+        rows.append([])
+
+    spawn_thread(play_music)
+
+    for i in xrange(0, 8):
+        spawn_thread(block_worker)
 
     camera = cv2.VideoCapture(0)
 
@@ -114,11 +133,7 @@ def main():
     while True:
         ret, frame = camera.read()
 
-        # Draw grid
-        for i in xrange(0,700):
-            for j in xrange(0,4):
-                frame[i][SQUARE_WIDTH*j] = (0,0,0)
-                frame[SQUARE_HEIGHT*j][i] = (0,0,0)
+        frame = cv2.medianBlur(frame, 5)
 
         if not CALIBRATING:
             results.clear()
@@ -130,16 +145,33 @@ def main():
 
             queue.join()
 
-            for row in rows:
-                del row[:]
+            new_rows = []
+            for i in range(0, NUM_ROWS):
+                new_rows.append([])
 
             for (i, j), counts in results.iteritems():
                 ordered = counts.most_common()
-                if ordered[0][0] != SQUARE_WHITE:
-                    rows[i].extend(counts_to_row(counts, j + 1))
+                if len(ordered) > 0 and ordered[0][1] > 300 and (ordered[0][0] != SQUARE_GREEN or ordered[0][1] > 550 or (len(ordered) > 1 and ordered[0][1] - ordered[1][1] < 200)):
+                    #print (i, j), map(lambda (n, c): ('red' if n == SQUARE_RED else ('blue' if n == SQUARE_BLUE else 'green'), c), ordered)
+                    new_rows[i].extend(counts_to_row(counts, j + 1))
 
-        # Uncomment the line below to see the feed
-        cv2.imshow('video', frame)
+            rows = new_rows
+
+            #print ''
+
+            sleep(0.5)
+
+        else:
+            # Draw grid
+            for i in xrange(0,NUM_COLS):
+                for j in xrange(0, len(frame[0])):
+                    frame[i * SQUARE_HEIGHT][j] = (0,0,0)
+
+            for j in xrange(0, NUM_ROWS):
+                for i in xrange(0, len(frame)):
+                    frame[i][j * SQUARE_WIDTH] = (0,0,0)
+
+            cv2.imshow('video', frame)
 
     del(camera)
     cv2.destroyAllWindows()
